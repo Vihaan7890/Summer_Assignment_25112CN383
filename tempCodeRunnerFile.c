3c4c7dffe87155931e0508698c57a96fc5f2c0f3
Vihaan7890 <vihaansachdeva778@gmail.com>
2026-05-28 22:49:44 +0530

    first commit

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,i,fact=1;
+    printf("enter the no :");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        fact*=i;
+    }
+    printf("the factorial of no:%d is-%d",n,fact);
+    return 0;
+}
