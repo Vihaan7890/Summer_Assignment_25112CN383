cdfa943388741cf96b4905746e57909b094c685f
Vihaan7890 <vihaansachdeva778@gmail.com>
2026-05-31 20:49:01 +0530

    fix Day-4 submodule issue

---
+++ Day-4/Q13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a=0, b=1, c, n, i;
+    printf("Enter the no: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++){
+        printf("%d\n", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
+
