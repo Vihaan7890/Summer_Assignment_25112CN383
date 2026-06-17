82e008b7f806711d05b64fe68a773e61c2a579c5
Vihaan7890 <vihaansachdeva778@gmail.com>
2026-06-17 14:48:58 +0530

    Add Q70.c and Q70.exe

---
+++ Day-18/Q70.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int i,j,n,a[100],temp;
+    printf("Enter the no of elemnets = ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(a[i]>a[j]){
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("Sorted Array by Selection Method = \n");
+    for(i=0;i<n;i++){
+        printf("%d\t",a[i]);
+    }
+    return 0;
+
+}
